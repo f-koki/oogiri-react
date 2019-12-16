a09d438ae2e8288fb17d19d7a3f48b7613565370
f-koki <fukushimakoki0108@gmail.com>
2019-12-16 22:33:23 +0900

    feat: implement Router

--- src/components/organisms/App/index.tsx
+++ src/components/organisms/App/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-import Button, { ButtonColor } from "../../atoms/Button";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Top from "../Top";
+import Login from "../Login";
 
 class App extends React.Component {
   render() {
-    return <Button color={ButtonColor.Main} message="hoge" />;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Top} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
