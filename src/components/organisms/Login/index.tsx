233e6ed1b3ba9b103307f01377caefee7fb6f484
f-koki <fukushimakoki0108@gmail.com>
2019-12-16 22:06:35 +0900

    add: add login and top

---
+++ src/components/organisms/Login/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// TODO
+
+type Props = {};
+
+type State = {};
+
+export default class Login extends React.Component<Props, State> {
+  render() {
+    return <div>ログイン画面</div>;
+  }
+}
---
+++ src/components/organisms/Top/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// TODO
+
+type Props = {};
+
+type State = {};
+
+export default class Top extends React.Component<Props, State> {
+  render() {
+    return <div>Top画面</div>;
+  }
+}
