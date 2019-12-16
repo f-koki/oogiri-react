667b1a0aef1387671d23295c1311e8a65c65bb93
f-koki <fukushimakoki0108@gmail.com>
2019-12-16 22:00:45 +0900

    fix: typescriptnize

---
+++ src/components/organisms/App/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Button, { ButtonColor } from "../../atoms/Button";
+
+class App extends React.Component {
+  render() {
+    return <Button color={ButtonColor.Main} message="hoge" />;
+  }
+}
+
+export default App;
