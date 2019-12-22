439450a0fa6eb91a74851999add9a8fc8005773a
f-koki <fukushimakoki0108@gmail.com>
2019-12-23 02:27:09 +0900

    feat: 満月をメニューボタンにする

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -1,18 +1,7 @@
 import React from "react";
-import Moon from "../../atoms/Moon";
 
 const Header: React.FC = () => {
-  return (
-    <header className="Header">
-      <div className="inner">
-        {/* TODO: Home icon作成 */}
-        <div className="home-icon">icon</div>
-        {/* TODO: バーガーメニュー作成 */}
-        <div className="burger-menue">burger</div>
-        <Moon />
-      </div>
-    </header>
-  );
+  return <header className="Header"></header>;
 };
 
 export default Header;
