2aabd725675fbad6d8aea24be40b7182fbc80b28
f-koki <57610613+f-koki@users.noreply.github.com>
2019-12-24 13:23:58 +0900

    feat: redirectの実装

--- src/components/organisms/Contents/index.tsx
+++ src/components/organisms/Contents/index.tsx
@@ -1,16 +1,34 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Top from "../Top";
 import Login from "../Login";
 import Answer from "../Answer";
+import { firebaseApp } from '../../../firebase'
 
 type Props = {};
 
 const Contents: React.FC<Props> = () => {
+  const [auth, setAuth] = useState<boolean>(false)
+  const [user, setUser] = useState<firebase.User | null>(null)
+
+  useEffect(() => {
+    firebaseApp.auth().onAuthStateChanged(user => {
+      if (user) {
+        setAuth(true)
+        setUser(user)
+      } else {
+        setAuth(false)
+        setUser(null)
+      }
+    })
+  })
+
   return (
     <div className="contents">
       <Switch>
-        <Route exact path="/" component={Top} />
+        <Route
+          exact path="/"
+          render={() => auth ? <Top /> : <Redirect to="/login" />} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/answer" component={Answer} />
       </Switch>
