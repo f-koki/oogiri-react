450867a7b49578daaa430bd23c127ea5507a3d49
f-koki <57610613+f-koki@users.noreply.github.com>
2019-12-24 17:44:10 +0900

    feat: ログイン・サインアップ機能の実装

--- src/components/organisms/Login/index.tsx
+++ src/components/organisms/Login/index.tsx
@@ -1,19 +1,43 @@
-import React from "react";
-import Form from "../../atoms/Form";
+import React, { useState } from "react";
+import { firebaseApp } from "../../../firebase";
+import { History } from "history";
 
-type Props = {};
+type Props = {
+  history: History
+};
 
-const Login: React.FC<Props> = () => {
+const Login: React.FC<Props> = (props: Props) => {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const handleClickLogin = async () => {
+    try {
+      await firebaseApp.auth().signInWithEmailAndPassword(email, password)
+      props.history.push('/')
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  const handleClickSignUp = async () => {
+    try {
+      await firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+      props.history.push('/')
+    } catch (error) {
+      alert(error)
+    }
+  }
+
   return (
     <div className="Login">
       <h1>ログイン画面</h1>
       <div>
-        <p>ログインID</p>
-        {/* <Form method="post" /> */}
-      </div>
-      <div>
-        <p>パスワード</p>
-        {/* <Form method="post" /> */}
+        <label htmlFor="email">Email</label>
+        <input id="email" type="text" value={email} onChange={e => { setEmail(e.currentTarget.value) }} />
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" value={password} onChange={e => { setPassword(e.currentTarget.value) }} />
+        <div onClick={handleClickLogin}>Login</div>
+        <div onClick={handleClickSignUp}>Sign Up</div>
       </div>
     </div>
   );
