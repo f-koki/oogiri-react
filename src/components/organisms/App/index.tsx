335b10cc7ec60fb8348bb3dea62173fea5eff3a4
f-koki <fukushimakoki0108@gmail.com>
2019-12-24 23:10:02 +0900

    feat: storeの実装

--- src/components/organisms/App/index.tsx
+++ src/components/organisms/App/index.tsx
@@ -3,22 +3,42 @@ import Header from "../../molecules/Header";
 import Contents from "../Contents";
 import Footer from "../../molecules/Footer";
 
-const store = {
-  name: "hoge",
-}
+type Props = {};
 
-export const RootContext = React.createContext(store)
+type Store = {
+  hoge?: string;
+};
 
-const App: React.FC = () => {
-  return (
-    <RootContext.Provider value={store}>
-      <div className="wrapper">
-        <Header />
-        <Contents />
-        <Footer />
-      </div>
-    </RootContext.Provider>
-  );
+type ContextProps = {
+  store: Store;
+  updateState: (key: string, value: any) => void;
+};
+
+export const RootContext = React.createContext({} as ContextProps);
+
+class App extends React.Component<Props, Store> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {};
+  }
+
+  updateState = (key: string, value: any) => {
+    this.setState({ [key]: value });
+  };
+
+  render() {
+    return (
+      <RootContext.Provider
+        value={{ store: this.state, updateState: this.updateState }}
+      >
+        <div className="wrapper">
+          <Header />
+          <Contents />
+          <Footer />
+        </div>
+      </RootContext.Provider>
+    );
+  }
 }
 
 export default App;
--- src/components/organisms/Login/index.tsx
+++ src/components/organisms/Login/index.tsx
@@ -4,47 +4,65 @@ import { History } from "history";
 import { RootContext } from "../App";
 
 type Props = {
-  history: History
+  history: History;
 };
 
 const Login: React.FC<Props> = (props: Props) => {
-  const [email, setEmail] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const { store, updateState } = useContext(RootContext);
 
   const handleClickLogin = async () => {
     try {
-      const userCredential = await firebaseApp.auth().signInWithEmailAndPassword(email, password)
+      const userCredential = await firebaseApp
+        .auth()
+        .signInWithEmailAndPassword(email, password);
       // TODO: user情報をクッキー or storeにセットする
-      props.history.push('/')
+      props.history.push("/");
     } catch (error) {
-      alert(error)
+      alert(error);
     }
-  }
+  };
 
   const handleClickSignUp = async () => {
     try {
-      await firebaseApp.auth().createUserWithEmailAndPassword(email, password)
-      props.history.push('/')
+      await firebaseApp.auth().createUserWithEmailAndPassword(email, password);
+      props.history.push("/");
     } catch (error) {
-      alert(error)
+      alert(error);
     }
-  }
-
-  const { name } = useContext(RootContext)
+  };
 
   return (
     <div className="Login">
-      <h1>ログイン画面{name}</h1>
+      {!store.hoge && updateState("hoge", "fuga")}
+      {console.log(store)}
+      <h1>ログイン画面</h1>
       <div>
         <label htmlFor="email">Email</label>
-        <input id="email" type="text" value={email} onChange={e => { setEmail(e.currentTarget.value) }} />
+        <input
+          id="email"
+          type="text"
+          value={email}
+          onChange={e => {
+            setEmail(e.currentTarget.value);
+          }}
+        />
         <label htmlFor="password">Password</label>
-        <input id="password" type="password" value={password} onChange={e => { setPassword(e.currentTarget.value) }} />
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={e => {
+            setPassword(e.currentTarget.value);
+          }}
+        />
         <div onClick={handleClickLogin}>Login</div>
         <div onClick={handleClickSignUp}>Sign Up</div>
       </div>
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
